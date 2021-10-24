c3d725b1e852757c649d81d72741a5652647c59c
rawand1998 <rawandgaradh@gmail.com>
2021-10-24 16:34:25 +0300

    fake data edit

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -1,46 +1,47 @@
 BEGIN;
-DROP TABLE IF EXISTS users, types, brands, cars, rentals CASCADE;
+DROP TABLE IF EXISTS users,
+types,
+brands,
+cars,
+rentals CASCADE;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-    is_admin BOOLEAN,
-    phone int
+    is_admin BOOLEAN DEFAULT false,
+    phone INT
 );
 CREATE TABLE types (
     id SERIAL PRIMARY KEY,
- name VARCHAR(100),
- image text
+    name VARCHAR(100),
+    image TEXT
 );
 CREATE TABLE brands (
     id SERIAL PRIMARY KEY,
- name VARCHAR(100),
- image text
+    name VARCHAR(100),
+    image TEXT
 );
 CREATE TABLE cars (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    door int,
-     types_id int REFERENCES types(id),
-     brands_id int REFERENCES brands(id),
-     release_year int,
+    door INT,
+    types_id INT REFERENCES types(id),
+    brands_id INT REFERENCES brands(id),
+    release_year INT,
     gearbox VARCHAR(100),
-    price  int,
-    seats int,
-    img_car text,
-    description text,
+    price INT,
+    seats INT,
+    img_car TEXT,
+    description TEXT,
     color VARCHAR(100)
 );
 CREATE TABLE rentals (
     id SERIAL PRIMARY KEY,
-    users_id int REFERENCES users(id),
-    cars_id int REFERENCES cars(id),
-    pick_up_date_time timestamp,
-    pick_of_date_time timestamp,
-    total_price int
+    users_id INT REFERENCES users(id),
+    cars_id INT REFERENCES cars(id),
+    pick_up_date_time TIMESTAMP,
+    pick_of_date_time TIMESTAMP,
+    total_price INT
 );
-
-
-
 COMMIT;
