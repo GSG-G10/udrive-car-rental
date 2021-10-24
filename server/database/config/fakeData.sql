cfa00f5037c47a44c644a55439a8f250923fb6da
rawand1998 <rawandgaradh@gmail.com>
2021-10-24 14:06:19 +0300

    edit hash password

--- server/database/config/fakeData.sql
+++ server/database/config/fakeData.sql
@@ -1,4 +1,4 @@
 
 INSERT INTO users (name,email,password, is_admin ,phone) VALUES
-('Rawand','rawandgaradh@Gmail.com','12345',false,0592320547),
+('Rawand','rawandgaradh@Gmail.com','144c9defac04969c7bfad8efaa8ea194',false,0592320547),
 ('Lina','lina@Gmail.com','lina',true,0592320547);
