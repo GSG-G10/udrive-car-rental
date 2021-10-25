0803fa148640c07d4e9dbc7922489bb667cf597e
rawand1998 <rawandgaradh@gmail.com>
2021-10-25 18:50:38 +0300

    make editing at getReview function

--- server/controllers/review/getReviewes.js
+++ server/controllers/review/getReviewes.js
@@ -1,10 +1,13 @@
+/* eslint-disable consistent-return */
 const { getReview } = require('../../database/queries/index');
 
-const getReviewes = (req, res) => {
-  const { carId } = req.params;
-  getReview(carId)
-    .then(({ rows }) => res.json({ data: rows[0] }))
-    .catch((err) => res.json({ status: 'err', error: err }));
+const getReviewes = async (req, res, next) => {
+  try {
+    const { carId } = req.params;
+    const getDataReview = await getReview(carId);
+    res.json(getDataReview.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
 };
-
 module.exports = { getReviewes };
