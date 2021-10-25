8393d2d44304158fdd0970211967d5057b52c028
rawand1998 <rawandgaradh@gmail.com>
2021-10-25 19:19:32 +0300

    make editing at login file

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -17,7 +17,7 @@ const login = async (req, res, next) => {
     if (!compared) {
       throw new Error({ message: 'invalid email or password', status: 401 });
     }
-    const token = await signTokenPromise(email, rows[0].id, rows[0].is_admin, rows[0].name);
+    const token = await signTokenPromise(email, rows[0].id, rows[0].name, rows[0].is_admin);
     res.cookie('token', token);
     return res.json({ message: 'logged in successfully' });
   } catch (err) {
