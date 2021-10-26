2fbeb0e3507a083a0e3352a17555ade51469c234
Abdallah <dev.aaamra@gmail.com>
2021-10-26 09:30:37 +0300

    custome error handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const { getTypeControllers } = require('../controllers/cars');
+const { clientError, serverError } = require('../controllers/errors');
 
 router.get('/type', getTypeControllers);
 const { login } = require('../controllers/index');
 
 router.post('/login', login);
+
+router.get(clientError);
+router.get(serverError);
 
 module.exports = router;
