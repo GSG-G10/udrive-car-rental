82b8fc9dac6e47d061dfe4c713441c8d41151c18
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-26 09:30:26 +0300

    change cookie to user #46

--- server/controllers/middleware/isAdmin.js
+++ server/controllers/middleware/isAdmin.js
@@ -1,5 +1,5 @@
 const isAdmin = (req, res, next) => {
-  if (req.cookies.isAdmin) return next();
+  if (req.user.isAdmin) return next();
   throw new Error({ message: 'Forbidden', status: 403 });
 };
 
