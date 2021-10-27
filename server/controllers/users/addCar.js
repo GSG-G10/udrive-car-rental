a6d290ba8e7867f9a254e38154625c667f6f8e5d
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-10-27 17:21:26 +0300

    validation async await

--- server/controllers/users/addCar.js
+++ server/controllers/users/addCar.js
@@ -2,27 +2,26 @@
 const {
   postAdminCarsQueries,
 } = require('../../database/queries/postAdminCarsQueries');
+const { boomify } = require('../../utils');
 
 const { addCarValidation } = require('../../utils');
 
 const addCar = async (req, res, next) => {
-  const {
-    name,
-    door,
-    typeId,
-    brandId,
-    releaseYear,
-    gearbox,
-    price,
-    seats,
-    imgCar,
-    description,
-    color,
-  } = req.body;
-
-  await addCarValidation.validateAsync(req.body);
-
   try {
+    const {
+      name,
+      door,
+      typeId,
+      brandId,
+      releaseYear,
+      gearbox,
+      price,
+      seats,
+      imgCar,
+      description,
+      color,
+    } = await addCarValidation.validateAsync(req.body);
+
     await postAdminCarsQueries(
       name,
       door,
@@ -36,8 +35,12 @@ const addCar = async (req, res, next) => {
       description,
       color,
     );
-    res.status(201).json({ message: 'INSERT DATA SUCCESSFULLY' });
+
+    return res.status(201).json({ message: 'INSERT DATA SUCCESSFULLY' });
   } catch (err) {
+    if (err.details) {
+      return next(boomify(422, 'Validation Error', err.details[0].message));
+    }
     return next(err);
   }
 };
