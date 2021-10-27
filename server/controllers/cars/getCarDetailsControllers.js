1fea9299e56576dc569574460f3e28874b896e4a
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-27 13:50:11 +0300

    create route test #3

--- server/controllers/cars/getCarDetailsControllers.js
+++ server/controllers/cars/getCarDetailsControllers.js
@@ -1,10 +1,14 @@
-const getCarDetailsQuery = require('../../database/queries');
+const { getCarDetailsQuery } = require('../../database/queries');
+const { boomify } = require('../../utils');
 
 const getCarDetailsController = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const { rows } = await getCarDetailsQuery(id);
-    res.json({ data: rows });
+    if (id > 0) {
+      const { rows } = await getCarDetailsQuery(id);
+      res.json({ data: rows });
+    }
+    throw boomify(400, 'Bad Request', 'Cant get car by id');
   } catch (err) {
     return next(err);
   }
---
+++ server/test/cars.test.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+const request = require('supertest');
+const app = require('../app');
+
+const { dbBuild } = require('../database/config/bulid');
+const connection = require('../database/connection');
+
+beforeEach(() => dbBuild());
+describe('Cars', () => {
+  test('get carDetails returns a status code of 200', (done) => {
+    request(app)
+      .get('/api/v1/cars/1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        const expected = {
+          data: [
+            {
+              id: 1,
+              name: '2020 Tesla Model 3',
+              door: 4,
+              types_id: 1,
+              brands_id: 1,
+              release_year: 2020,
+              gearbox: 'Helical gearbox.',
+              price: 150,
+              seats: 5,
+              img_car: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2019-tesla-model-3-101-1574807022.jpg?crop=0.745xw:0.610xh;0.0208xw,0.387xh&resize=1200:*',
+              description: 'Tesla has singlehandedly changed the electric-car landscape, and the Model 3 is the least expensive way to get one of the companys tech wonders in your driveway.',
+              color: 'black',
+              types_name: 'Seden',
+              brands_name: 'Tesla',
+            },
+          ],
+        };
+        const actual = res.body;
+        expect(actual).toEqual(expected);
+        return done();
+      });
+  });
+});
+
+afterAll(() => connection.end());
