28ffb879e1f80de55f11be1d16adfd82e0640db7
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-28 00:41:56 +0300

    change snake case to camel case #87

--- server/utils/validation/rentalValidation.js
+++ server/utils/validation/rentalValidation.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const rentalValidation = Joi.object({
-  pick_up_date_time: Joi.string().isoDate(),
-  pick_of_date_time: Joi.string().isoDate,
-  total_price: Joi.number().positive(),
+  pickUpDateTime: Joi.string().isoDate(),
+  pickOffDateTime: Joi.string().isoDate,
+  totalPrice: Joi.number().positive(),
 });
 
 module.exports = rentalValidation;
