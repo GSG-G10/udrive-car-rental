463c34554da5914f58c3d95941a789310e7df872
rawand1998 <rawandgaradh@gmail.com>
2021-10-27 11:43:28 +0300

    make a brand valaidtion

--- server/controllers/admin/postBrand.js
+++ server/controllers/admin/postBrand.js
@@ -1,14 +1,26 @@
 const { postBrands } = require('../../database/queries/index');
+const { brandValiadtion } = require('../../utils');
+const { boomify } = require('../../utils/index');
 
 const postBrand = async (req, res, next) => {
-  const { name, image } = req.body;
   try {
+    const { name, image } = await brandValiadtion.validateAsync(
+      req.body,
+    );
     const { rows: data } = await postBrands(name, image);
     res.status(201).json({
       message: 'successfully post brand',
       data,
     });
   } catch (err) {
+    if (err.details) {
+      return next(boomify(
+        422,
+        'validation error',
+        err.details[0].message,
+      ));
+    }
+
     next(err);
   }
 };
