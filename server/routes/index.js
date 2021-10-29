4fe2edee80d260c0a2746428817d0a5490ecc4b0
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-29 22:55:35 +0300

    remove testing route #87

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,9 +25,7 @@ router.post('/type', isAuth, isAdmin, postType);
 router.post('/rentals/:carId', isAuth, addRentalController);
 router.get('/brands', getBrands);
 router.get('/logout', logout);
-router.get('/test', isAuth, (req, res) => {
-  res.json(req.user);
-});
+
 router.use(clientError);
 router.use(serverError);
 
