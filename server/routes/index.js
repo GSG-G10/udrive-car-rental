5cd2af2d8e0176052840f11169c6f58d014bbc17
Abdallah <dev.aaamra@gmail.com>
2021-10-29 19:50:43 +0300

    create delete rental endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ const {
   getBrands,
   getReviewes,
   postBrand,
-  isAuth, isAdmin, signUp, logout, postType, deleteCar,
+  isAuth, isAdmin, signUp, logout, postType, deleteCar, deleteRental,
 } = require('../controllers');
 
 router.get('/auth/user', isAuth, authUser);
@@ -26,6 +26,7 @@ router.post('/type', isAuth, isAdmin, postType);
 router.get('/brands', getBrands);
 router.get('/logout', logout);
 router.delete('/admin/deleteCar/carId', isAuth, isAdmin, deleteCar);
+router.delete('/rentals/:rentalId', isAuth, deleteRental);
 router.use(clientError);
 router.use(serverError);
 
