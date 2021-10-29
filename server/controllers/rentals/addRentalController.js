8a40cccd93fac35325ab34cc779bdfd03fb20e5a
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-29 22:47:56 +0300

    create tests #87

--- server/controllers/rentals/addRentalController.js
+++ server/controllers/rentals/addRentalController.js
@@ -1,23 +1,37 @@
+const moment = require('moment');
+
 const { boomify, rentalValidation } = require('../../utils');
 const { checkDateTimeQuery, addRentalQuery } = require('../../database/queries');
 
 const addRentalController = async (req, res, next) => {
-  const userId = 2;
+  const { carId } = req.params;
+  const id = 2;
   try {
     const {
-      carId,
+      location,
       pickUpDateTime,
       pickOffDateTime,
     } = await rentalValidation.validateAsync(req.body);
 
-    const { rows } = await checkDateTimeQuery(carId, pickUpDateTime, pickOffDateTime);
+    const pickup = moment(pickUpDateTime);
+    const pickoff = moment(pickOffDateTime);
 
+    if (moment.duration(pickoff.diff(pickup)) <= 0) {
+      throw (boomify(400, 'Bad Request!', 'PickOff date must be larger than PickUp date'));
+    }
+
+    const { rows } = await checkDateTimeQuery(carId, pickUpDateTime, pickOffDateTime);
     if (rows.length) {
       throw (boomify(422, 'Validation Error', 'Car is in use'));
     }
-    await addRentalQuery(userId, carId, pickUpDateTime, pickOffDateTime);
 
-    return res.status(200).json({ message: 'Car rented successfully' });
+    // Calculate the total hours to calculate the total price (hours * pricePerHour)
+    const duration = moment.duration(pickoff.diff(pickup));
+    const hours = duration.asHours();
+
+    await addRentalQuery(id, carId, location, pickUpDateTime, pickOffDateTime, hours);
+
+    return res.status(201).json({ message: 'Car rented successfully' });
   } catch (err) {
     console.log(err);
     next(err);
--- server/routes/index.js
+++ server/routes/index.js
@@ -22,7 +22,7 @@ router.get('/type', getTypeControllers);
 router.post('/login', login);
 router.post('/brand', isAuth, isAdmin, postBrand);
 router.post('/type', isAuth, isAdmin, postType);
-router.post('/rentals', addRentalController);
+router.post('/rentals/:carId', isAuth, addRentalController);
 router.get('/brands', getBrands);
 router.get('/logout', logout);
 router.get('/test', isAuth, (req, res) => {
