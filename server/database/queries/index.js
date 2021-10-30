327c6d8ddc323b4e842b0b45628e77d4489fda5d
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-30 10:30:12 +0200

    create delete query #7

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -3,11 +3,18 @@ const { getReview } = require('./getReview');
 
 const { getTypeQueries } = require('./getTypeQueries');
 const { postBrands } = require('./postBrands');
+const deleteBrandQuery = require('./deleteBrandQuery');
 const checkEmail = require('./checkEmail');
 const { postTypes } = require('./postTypes');
 const signUpQuery = require('./users/signUpQuery');
 
 module.exports = {
-  checkEmail, signUpQuery, getTypeQueries, getReview, postBrands, postTypes, getBrandsQueries,
-
+  checkEmail,
+  signUpQuery,
+  getTypeQueries,
+  getReview,
+  postBrands,
+  postTypes,
+  getBrandsQueries,
+  deleteBrandQuery,
 };
