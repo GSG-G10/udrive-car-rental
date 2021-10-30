2b6a668d57cfa2917d3f1518fbe69780afaa00bf
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-30 18:27:51 +0200

    write PickOff in camelcase #87

--- server/database/queries/rentals/addRentalQuery.js
+++ server/database/queries/rentals/addRentalQuery.js
@@ -1,10 +1,10 @@
 const connection = require('../../connection');
 
-const addRentalQuery = (userId, carId, location, pickUpDateTime, PickOffDateTime, hours) => {
+const addRentalQuery = (userId, carId, location, pickUpDateTime, pickOffDateTime, hours) => {
   const sql = {
     text: `INSERT INTO rentals (users_id, cars_id, location, pick_up_date_time, pick_of_date_time,total_price) 
     VALUES ($1, $2, $3, $4, $5, (SELECT price FROM cars WHERE id = $2) * $6) RETURNING *;`,
-    values: [userId, carId, location, pickUpDateTime, PickOffDateTime, hours],
+    values: [userId, carId, location, pickUpDateTime, pickOffDateTime, hours],
   };
 
   return connection.query(sql);
