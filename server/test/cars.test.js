e90773f49ecf56d261366e3e50ea236202e23cc6
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-30 18:23:30 +0200

    test 400 status code case #3

--- server/test/cars.test.js
+++ server/test/cars.test.js
@@ -9,7 +9,7 @@ beforeEach(() => dbBuild());
 describe('Cars', () => {
   test('get carDetails returns a status code of 200', (done) => {
     request(app)
-      .get('/api/v1/cars/1')
+      .get('/api/v1/cars/3')
       .expect(200)
       .expect('Content-Type', /json/)
       .end((err, res) => {
@@ -17,26 +17,40 @@ describe('Cars', () => {
         const expected = {
           data: [
             {
-              id: 1,
-              name: '2020 Tesla Model 3',
+              id: 3,
+              name: 'FerrariSF90Stradale',
               door: 4,
-              types_id: 1,
-              brands_id: 1,
-              release_year: 2020,
-              gearbox: 'Helical gearbox.',
-              price: 150,
-              seats: 5,
-              img_car: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2019-tesla-model-3-101-1574807022.jpg?crop=0.745xw:0.610xh;0.0208xw,0.387xh&resize=1200:*',
-              description: 'Tesla has singlehandedly changed the electric-car landscape, and the Model 3 is the least expensive way to get one of the companys tech wonders in your driveway.',
-              color: 'black',
-              type: 'Seden',
-              brand: 'Tesla',
-              rate: '5.0000000000000000',
+              types_id: 3,
+              brands_id: 3,
+              release_year: 2021,
+              gearbox: 'Bevel helical gearbox.',
+              price: 110,
+              seats: 4,
+              img_car: 'https://carconfigurator.ferrari.com/assets/cars/portofinom/packages/default/car-ferrari-portofino-m_splash.jpg',
+              description: 'With three electric motors and a 3.9-liter V8 engine, the new hybrid Ferrari can glide silently through city streets — and reach a top speed of 340 kilometers per hour.',
+              color: 'gray',
+              type: 'Sports Car',
+              brand: 'Ferrai',
+              rate: '4.0000000000000000',
+              pick_up_date_time: '2021-12-26T23:00:00.000Z',
+              pick_of_date_time: '2021-12-27T01:00:00.000Z',
             },
           ],
         };
         const actual = res.body;
         expect(actual).toEqual(expected);
+        return done();
+      });
+  });
+
+  test('get carDetails returns a status code of 400', (done) => {
+    request(app)
+      .get('/api/v1/cars/-1')
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).toBe(400);
         return done();
       });
   });
