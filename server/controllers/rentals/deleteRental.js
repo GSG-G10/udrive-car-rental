c0299cc167de0541535bbdda42c252f5b444a157
Abdallah <dev.aaamra@gmail.com>
2021-10-30 16:33:28 +0200

    fix requested changes

--- server/controllers/rentals/deleteRental.js
+++ server/controllers/rentals/deleteRental.js
@@ -1,16 +1,16 @@
 const { deleteRentalQuery } = require('../../database/queries');
-const boomify = require('../../utils/boomify');
+const { boomify } = require('../../utils');
 
 const deleteRental = async (req, res, next) => {
   const { rentalId } = req.params;
   try {
     const { rows } = await deleteRentalQuery(rentalId);
     if (rows.length) {
-      res.json({ status: 200, message: 'rental deleted successfully !' });
+      return res.json({ status: 200, message: 'rental deleted successfully !' });
     }
     throw boomify(400, 'Bad Request', 'Some thin went wrong !');
   } catch (err) {
-    next(err);
+    return next(err);
   }
 };
 
