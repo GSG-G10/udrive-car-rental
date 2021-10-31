561e349cd125629cad77b293d48d535730368686
rawand1998 <rawandgaradh@gmail.com>
2021-10-31 14:04:55 +0200

    add test brand

--- server/test/brands.test.js
+++ server/test/brands.test.js
@@ -11,6 +11,19 @@ const { TOKEN } = process.env;
 beforeAll(() => dbBuild());
 
 afterAll(() => connection.end());
+
+test('get brands returns a status code of 200', (done) => {
+  request(app)
+    .get('/api/v1/brands')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(res.body.data.length).toBe(5);
+      return done();
+    });
+});
+
 test('add new brand returns 201', (done) => {
   request(app)
     .post('/api/v1/admin/brands')
