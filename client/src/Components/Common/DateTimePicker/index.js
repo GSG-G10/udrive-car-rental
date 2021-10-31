2eb3435566693b6f78d87454e5fb2d1a560a1f17
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-31 20:37:18 +0200

    create date picker common component #118

---
+++ client/src/Components/Common/DateTimePicker/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { TextField } from '@mui/material';
+import moment from 'moment';
+
+export default function DateTimePickerComp() {
+  const [dateTime, setDateTime] = useState(moment().format('yyyy-MM-DDThh:mm'));
+
+  return (
+    <TextField
+      id="datetime-local"
+      label="Date&Time Picker"
+      type="datetime-local"
+      defaultValue={dateTime}
+      sx={{ width: 250 }}
+      InputLabelProps={{
+        shrink: true,
+      }}
+      onChange={(newValue) => {
+        setDateTime(newValue.target.value);
+      }}
+    />
+  );
+}
