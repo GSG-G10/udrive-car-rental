7cd6ec2ffc63fbb7751f399b2e9f48840133d80e
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-10-31 11:54:10 +0200

    create react router

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './style.css';
+import { Switch, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>Hello World !</h1>
+      <Switch>
+        <Router expect path="/">
+          home page
+        </Router>
+        <Router expect path="/dashboard">
+          dashboard page
+        </Router>
+        <Router expect path="/cars">
+          cars page
+        </Router>
+        <Router expect path="/carDetails">
+          Car Details page
+        </Router>
+        <Router expect path="/booking">
+          Booking page
+        </Router>
+        <Router expect path="/login">
+          login page
+        </Router>
+        <Router expect path="/signup">
+          signup page
+        </Router>
+        <Router expect path="/login">
+          login page
+        </Router>
+        <Router expect path="/addCar">
+          add cars page
+        </Router>
+      </Switch>
     </div>
   );
 }
