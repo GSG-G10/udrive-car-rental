dc8fd98911a36fa995feedbc3e8d49631168c41e
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-11-01 18:24:52 +0200

    Header in progress

--- client/src/Components/Common/Header/index.jsx
+++ client/src/Components/Common/Header/index.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import './style.css';
 
 function Header() {
-  return <div>hello world from Header</div>;
+  return (
+    <div className="img">
+      <div className="container">
+        <div className="header">
+          <h1>First Class Car Rental & Limousine Services</h1>
+        </div>
+        <div className="description">
+          <p>
+            We offer professional car rental & limousine services in our range
+            of high-end vehicles
+          </p>
+        </div>
+        <div className="time">
+          <div
+            style={{
+              width: '200px',
+              height: '50px',
+              backgroundColor: 'red',
+            }}
+          />
+          <div
+            style={{
+              width: '200px',
+              height: '50px',
+              backgroundColor: 'red',
+            }}
+          />
+          <div
+            style={{
+              width: '200px',
+              height: '50px',
+              backgroundColor: 'red',
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
