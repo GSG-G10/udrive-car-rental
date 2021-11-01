bbae7fe8092df4120f46c19bd6f9db551cdd882d
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-11-01 13:05:26 +0200

    remove datetimepicker from App.js #118

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './style.css';
-import DateTimePicker from '../Components/Common/DateTimePicker';
 
 function App() {
   return (
     <div>
       <h1>Hello World !</h1>
-      <DateTimePicker />
     </div>
   );
 }
