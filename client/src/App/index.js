fb6987f5925a2a6f631d9472a3f2f1d9286c78c6
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-11-01 13:36:07 +0200

    initial Header component

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.css';
+import Header from '../Components/Common/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World !</h1>
+    <div className="App">
+      <Header />
     </div>
   );
 }
