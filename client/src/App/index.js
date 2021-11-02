520fd0c63374acf4267153477e793e6b15246db4
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-11-02 12:39:58 +0200

    remove the trying carCard from the App.js #14

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './style.css';
-import CarCard from '../Components/Common/CarCard';
 
 function App() {
   return (
     <div>
       <h1>Hello World !</h1>
-      <CarCard
-        image="https://www.mclarenofgreenwich.com/imagetag/3008/main/l/New-2022-Ferrari-Portofino-M.jpg"
-        carName="Ferrari"
-        rating={4}
-        gearbox="Automatic gearbox"
-        doors={3}
-        seats={4}
-        price={70}
-      />
     </div>
   );
 }
