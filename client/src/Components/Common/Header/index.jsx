73c5667bfc4919f09698fc49ad49bb1c3caa3acd
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-11-02 12:24:46 +0200

    remove divs and approval children

--- client/src/Components/Common/Header/index.jsx
+++ client/src/Components/Common/Header/index.jsx
@@ -2,33 +2,19 @@ import React from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
 
-function Header({
-  img, header, description, children,
-}) {
+function Header({ img, children }) {
   return (
     <div className="img" style={{ backgroundImage: `url(${img})` }}>
-      <div className="container">
-        <div className="header">
-          <h1>{header}</h1>
-        </div>
-        <div className="description">
-          <p>{description}</p>
-        </div>
-        <div className="component">{children}</div>
-      </div>
+      <div className="component">{children}</div>
     </div>
   );
 }
 Header.defaultProps = {
   img: '',
-  header: '',
-  description: '',
   children: '',
 };
 Header.propTypes = {
   img: PropTypes.string,
-  header: PropTypes.string,
-  description: PropTypes.string,
   children: PropTypes.node,
 };
 
