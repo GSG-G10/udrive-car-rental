52b34d070a507c8a13d65529efb649ce0c6fa546
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-11-03 12:14:19 +0200

    reomve css file and add avatar

--- client/src/Components/Common/CustomersOpinion/index.jsx
+++ client/src/Components/Common/CustomersOpinion/index.jsx
@@ -4,13 +4,18 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import PropTypes from 'prop-types';
+import Avatar from '@mui/material/Avatar';
 
-function CustomerOpinion({ userName, rateNumber, description }) {
+function CustomersOpinion({
+  userImg, userName, rateNumber, description,
+}) {
   return (
     <div sx={{ maxWidth: 275 }}>
       <Card sx={{ maxWidth: 275, margin: '20px' }}>
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <Typography sx={{ fontSize: 14, display: 'flex' }} color="text.secondary" gutterBottom>
+            <Avatar alt="Remy Sharp" src={userImg} />
+            {' '}
             {userName}
           </Typography>
           <Typography variant="h5" component="div">
@@ -25,10 +30,15 @@ function CustomerOpinion({ userName, rateNumber, description }) {
   );
 }
 
-CustomerOpinion.propTypes = {
+CustomersOpinion.defaultProps = {
+  userImg: '',
+};
+
+CustomersOpinion.propTypes = {
+  userImg: PropTypes.string,
   userName: PropTypes.string.isRequired,
   rateNumber: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
 };
 
-export default CustomerOpinion;
+export default CustomersOpinion;
