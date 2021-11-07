904b7605b6d1672b9b8274a518fd8ff96abd872b
Abdallah <dev.aaamra@gmail.com>
2021-11-07 22:30:27 +0200

    dashboard cars page

--- client/src/Pages/Dashboard/Cars/index.js
+++ client/src/Pages/Dashboard/Cars/index.js
@@ -11,7 +11,6 @@ import './style.css';
 function Cars() {
   const [cars, setCars] = useState([]);
   const [openDialog, setOpenDialog] = useState(false);
-  const [car, setCar] = useState({});
 
   const handleSave = () => {};
 
